3f1ff11be3e08faa708c6737121d61adc71ea25d
Carine Dengler <git@carine-dengler.de>
2025-06-08 18:01:37 +0200

    feat: add Docker engine "System" API calls

---
+++ shearwater/docker.py
@@ -0,0 +1,157 @@
+#    This file is part of Shearwater.
+#    Copyright (C) 2025  Carine Dengler
+#
+#    Shearwater is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+:synopsis: Docker API.
+"""
+
+
+# standard library imports
+import os
+import json
+import select
+import socket
+import email.parser
+
+# third party imports
+# library specific imports
+
+
+# https://github.com/python/cpython/blob/1a07a01014bde23acd2684916ef38dc0cd73c2de/Lib/multiprocessing/connection.py#L42
+HOST = "localhost"
+# https://www.rfc-editor.org/rfc/rfc2616#section-2.2
+CR_LF = b"\r\n"
+
+
+DOCKER_DAEMON_SOCKET = "/var/run/docker.sock"
+API_VERSION = "/v1.49"
+
+
+INFO = f"{API_VERSION}/info"
+VERSION = f"{API_VERSION}/version"
+EVENTS = f"{API_VERSION}/events"
+DF = f"{API_VERSION}/system/df"
+
+
+def readfile(fp):
+    """Read file object associated with the socket.
+
+    :param fp: file object
+
+    :returns: HTTP response
+    :rtype: bytes
+    """
+    response = []
+    chunked_transfer = False
+    # read start line
+    while True:
+        line = fp.readline()
+        if not line:
+            return b"".join(response)
+        response.append(line)
+        if line == CR_LF:
+            break
+        protocol, status_code, _ = line.split(b" ")
+        if protocol != b"HTTP/1.1":
+            raise NotImplementedError
+        break
+    # read headers
+    while True:
+        line = fp.readline()
+        if not line:
+            return b"".join(response)
+        response.append(line)
+        if line == CR_LF:
+            break
+        name, value = line.split(b":", 1)
+        value = value.strip()
+        chunked_transfer = (
+            name == b"Transfer-Encoding"
+            and value == b"chunked"
+        )
+    # read body
+    if chunked_transfer:
+        while True:
+            line = fp.readline()
+            if not line:
+                return b"".join(response)
+            try:
+                chunk_size = int(line, base=16)
+            except ValueError:
+                continue
+            line = fp.readline()
+            if not line:
+                return b"".join(response)
+            response.append(
+                line[:chunk_size] if chunk_size > 0 else line
+            )
+            if chunk_size == 0:
+                break
+        # read trailer
+        while True:
+            line = fp.readline()
+            if not line:
+                return b"".join(response)
+            response.append(line)
+    if not chunked_transfer:
+        while True:
+            line = fp.readline()
+            if not line:
+                return b"".join(response)
+            response.append(line)
+
+
+def send(path):
+    """Send HTTP request on Docker daemon socket.
+
+    :param str path: path
+
+    :returns: HTTP response
+    :rtype: bytes
+    """
+    request = f"GET {path} HTTP/1.1{os.linesep}Host: {HOST}{2 * os.linesep}".encode()   # noqa: E501
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+        sock.setblocking(False)
+        sock.connect(DOCKER_DAEMON_SOCKET)
+        _, wsock, _ = select.select([], [sock], [])
+        wsock[0].sendall(request)
+        rsock, _, _ = select.select([sock], [], [])
+        with rsock[0].makefile(mode="rb", newline=CR_LF) as fp:
+            return readfile(fp)
+
+
+def parse_http_response(response):
+    """Parse HTTP response.
+
+    :param bytes response: HTTP response
+
+    :returns: HTTP response
+    :rtype: dict
+    """
+    start_line, response = response.split(CR_LF, 1)
+    headers, response = response.split(2 * CR_LF, 1)
+    try:
+        body, additional_headers = response.split(2 * CR_LF, 1)
+        headers = additional_headers
+    except ValueError:
+        body = response
+    body = body.replace(CR_LF, b"").decode()
+    body = json.loads(body) if body else {}
+    headers = dict(
+        email.parser.BytesHeaderParser().parsebytes(headers).items()
+    )
+    return {"headers": headers, "body": body}
---
+++ tests/test_docker.py
@@ -0,0 +1,62 @@
+#    This file is part of Shearwater.
+#    Copyright (C) 2025  Carine Dengler
+#
+#    Shearwater is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+:synopsis: Docker API test cases.
+"""
+
+
+# standard library imports
+import json
+import unittest
+
+# third party imports
+# library specific imports
+import shearwater.docker
+
+from tests import DATA_DIR
+
+
+class TestDockerAPI(unittest.TestCase):
+    """Test Docker API."""
+
+    def test_readfile(self):
+        """Test reading file object associated with the socket.
+
+        Trying: raw HTTP response
+        Expecting: decoded HTTP response
+        """
+        for name in ("df.txt", "events.txt", "info.txt", "version.txt"):
+            with open(DATA_DIR / "decoded" / name, mode="rb") as fp:
+                expected = b"".join(fp.readlines())
+            with open(DATA_DIR / "raw" / name, mode="rb") as fp:
+                actual = shearwater.docker.readfile(fp)
+            self.assertEqual(actual, expected)
+
+    def test_parse_http_response(self):
+        """Test parsing HTTP response.
+
+        Trying: decoded HTTP response
+        Expecting: parsed HTTP response
+        """
+        for name in ("df", "events", "info", "version"):
+            with open(DATA_DIR / "decoded" / f"{name}.txt", mode="rb") as fp:
+                response = b"".join(fp.readlines())
+            actual = shearwater.docker.parse_http_response(response)
+            with open(DATA_DIR / "parsed" / f"{name}.json") as fp:
+                expected = json.load(fp)
+            self.assertEqual(actual, expected)
